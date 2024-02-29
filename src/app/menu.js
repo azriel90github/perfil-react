43566d0c05eac8ab20097ee67296b2679881ecd5
azriel90github <azrielgithub@gmail.com>
2024-02-29 18:20:21 +0000

    menu-react

--- src/app/menu.js
+++ src/app/menu.js
@@ -6,13 +6,18 @@ export function Menu() {
         <>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Bootstrap demo</title>
+            <title>Perfil React</title>
             <link
+
+                
+
                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
                 rel="stylesheet"
                 integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
                 crossOrigin="anonymous"
             />
+
+
             
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
